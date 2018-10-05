539adb8c5fca006c61aecc3cc44afb597898eaaa
Codeyluke <lukekennedy212@gmail.com>
2018-10-06 02:49:23 +0800

    Enter

--- src/Routes.js
+++ src/Routes.js
@@ -1,9 +1,18 @@
 import React, { Component } from "react";
 import Layout from "./HOC/Layout";
+import { Switch, Route } from "react-router-dom";
+
+import Home from "./Components/Home";
 
 class Routes extends Component {
   render() {
-    return <Layout>second line</Layout>;
+    return (
+      <Layout>
+        <Switch>
+          <Route exact component={Home} path="/" />
+        </Switch>
+      </Layout>
+    );
   }
 }
 
