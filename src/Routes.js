1787a7f3f73a49e45edaf06755c7ffc69be39dbf
Codeyluke <lukekennedy212@gmail.com>
2018-10-09 19:27:09 +0800

    private and public routes

--- src/Routes.js
+++ src/Routes.js
@@ -1,8 +1,9 @@
 import React from "react";
 import Layout from "./HOC/Layout";
-import { Switch, Route } from "react-router-dom";
+import { Switch } from "react-router-dom";
 
 import PrivateRoute from "./Components/AuthRoutes/PrivateRoutes";
+import PublicRoute from "./Components/AuthRoutes/PublicRoutes";
 
 import Home from "./Components/Home";
 import SignIn from "./Components/SignIn";
@@ -19,9 +20,20 @@ const Routes = props => {
           exact
           component={Dashboard}
         />
-
-        <Route exact component={SignIn} path="/sign_in" />
-        <Route exact component={Home} path="/" />
+        <PublicRoute
+          {...props}
+          path="/sign_in"
+          restricted={true}
+          exact
+          component={SignIn}
+        />
+        <PublicRoute
+          {...props}
+          path="/"
+          restricted={false}
+          exact
+          component={Home}
+        />
       </Switch>
     </Layout>
   );
