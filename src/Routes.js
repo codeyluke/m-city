26da10e7a2b55fb8a63ae1a83b4427464286bac3
Codeyluke <lukekennedy212@gmail.com>
2018-10-09 18:34:02 +0800

    enter

--- src/Routes.js
+++ src/Routes.js
@@ -1,24 +1,30 @@
-import React, { Component } from "react";
+import React from "react";
 import Layout from "./HOC/Layout";
 import { Switch, Route } from "react-router-dom";
+
+import PrivateRoute from "./Components/AuthRoutes/PrivateRoutes";
 
 import Home from "./Components/Home";
 import SignIn from "./Components/SignIn";
 
 import Dashboard from "./Components/Admin/Dashboard";
 
-class Routes extends Component {
-  render() {
-    return (
-      <Layout>
-        <Switch>
-          <Route exact component={Dashboard} path="/dashboard" />
-          <Route exact component={SignIn} path="/sign_in" />
-          <Route exact component={Home} path="/" />
-        </Switch>
-      </Layout>
-    );
-  }
-}
+const Routes = props => {
+  return (
+    <Layout>
+      <Switch>
+        <PrivateRoute
+          {...props}
+          path="/dashboard"
+          exact
+          component={Dashboard}
+        />
+
+        <Route exact component={SignIn} path="/sign_in" />
+        <Route exact component={Home} path="/" />
+      </Switch>
+    </Layout>
+  );
+};
 
 export default Routes;
